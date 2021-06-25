3a7f36c42f08affa183eb4baa955cb305e477a79
goperzin <leandrolopes.help@gmail.com>
2021-06-25 03:14:38 -0300

    Crud de funcionario finalizado

--- api-dance/src/main/java/br/com/apidance/controller/FuncionarioController.java
+++ api-dance/src/main/java/br/com/apidance/controller/FuncionarioController.java
@@ -3,10 +3,13 @@ package br.com.apidance.controller;
 import java.net.URI;
 import java.util.List;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -26,9 +29,8 @@ public class FuncionarioController {
 	@Autowired
 	FuncionarioRepository funcionarioRepository;
 	
-	@PostMapping("cadastrar")
+	@PostMapping()
 	public ResponseEntity<FuncionarioDTO> cadastrar(@RequestBody FuncionarioForm form, UriComponentsBuilder uriBuilder) {
-
 		Funcionario funcionario = new Funcionario(form);
 		funcionarioRepository.save(funcionario);
 		
@@ -37,19 +39,31 @@ public class FuncionarioController {
 		return ResponseEntity.created(uri).body(new FuncionarioDTO(funcionario));
 	}
 	
-	@GetMapping("listar")
+	@GetMapping()
 	public List<Funcionario> listar() {
 		return funcionarioRepository.findAll();
 	}
 	
-	@PutMapping("alterar")
-	public List<Funcionario> alterar() {
-		return funcionarioRepository.findAll();
+	@GetMapping("/{id}")
+	public FuncionarioDTO listarPorId(@PathVariable Long id) {
+		Funcionario funcionario = funcionarioRepository.getById(id);
+		
+		return new FuncionarioDTO(funcionario);
+	}
+	
+	@PutMapping("/{id}")
+	@Transactional
+	public ResponseEntity<FuncionarioDTO> alterar(@PathVariable Long id, @RequestBody FuncionarioForm form) {
+		Funcionario funcionario = funcionarioRepository.getById(id);
+		funcionario.setNome(form.getNome());
+		
+		return ResponseEntity.ok(new FuncionarioDTO(funcionario));
 	}
 
-	@DeleteMapping("desativar")
-	public List<Funcionario> desativar() {
-		return funcionarioRepository.findAll();
+	@DeleteMapping("/{id}")
+	public ResponseEntity<?> remover(@PathVariable Long id) {
+		funcionarioRepository.deleteById(id);
+		return ResponseEntity.ok().build();
 	}
 	
 	
